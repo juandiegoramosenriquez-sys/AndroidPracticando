2ce1bd8e763a75ca1a943a1f1654be2dbf1477c5
Juan Diego Ramos Enriquez <juandiegoramosenriquez@gmail.com>
2026-08-26 23:18:09 -0500

    Agrega botón Presióname a la pantalla principal

--- app/src/main/java/com/ramos/androidpracticando/MainActivity.kt
+++ app/src/main/java/com/ramos/androidpracticando/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -37,6 +38,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         Text(text = "Hola, soy $name", fontSize = 24.sp)
         Text(text = "Curso: Programación en Móviles")
+
+        Button(onClick = { }) {
+            Text("Presióname")
+        }
     }
 }
 
